255ea419cbe6c74d13932680a9a2f204068f7f0c
y-l-mochizuki <mczk9402@gmail.com>
2024-03-02 17:29:51 +0900

    Revert "add: basic auth"
    This reverts commit 50d8709ae9ce2a5b9169dc42b87b70da33860a45.

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const USER_NAME = "test";
+const PASSWORD = "test";
+
+export const runtime = "edge";
+
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get("authorization");
+  if (basicAuth) {
+    const auth = basicAuth.split(" ")[1];
+    const [user, password] = atob(auth).split(":");
+    if (user === USER_NAME && password === PASSWORD) {
+      return NextResponse.next();
+    }
+  }
+
+  return new Response("Auth required", {
+    status: 401,
+    headers: {
+      "WWW-authenticate": 'Basic realm="Secure Area"',
+    },
+  });
+}

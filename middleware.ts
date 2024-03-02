51092f37bcdc470aa4e5e3d86741bd046e8c04c5
y-l-mochizuki <mczk9402@gmail.com>
2024-03-02 18:10:09 +0900

    fix

--- middleware.ts
+++ middleware.ts
@@ -3,20 +3,26 @@ import { next } from "@vercel/edge";
 const USER_NAME = "test";
 const PASSWORD = "test";
 
-export function middleware(req: Request) {
-  const basicAuth = req.headers.get("authorization");
-  if (basicAuth) {
-    const auth = basicAuth.split(" ")[1];
-    const [user, password] = atob(auth).split(":");
+export const config = {
+  matcher: "/",
+};
+
+export default function middleware(request: Request) {
+  const authorizationHeader = request.headers.get("authorization");
+
+  if (authorizationHeader) {
+    const basicAuth = authorizationHeader.split(" ")[1];
+    const [user, password] = atob(basicAuth).toString().split(":");
+
     if (user === USER_NAME && password === PASSWORD) {
       return next();
     }
   }
 
-  return new Response("Auth required", {
+  return new Response("Basic Auth required", {
     status: 401,
     headers: {
-      "WWW-authenticate": 'Basic realm="Secure Area"',
+      "WWW-Authenticate": 'Basic realm="Secure Area"',
     },
   });
 }

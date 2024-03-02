ffe6b5cfc06cc0aa05629ce91a17a60fe12aa2f7
y-l-mochizuki <mczk9402@gmail.com>
2024-03-02 16:30:52 +0900

    revert: app.js vercel.json

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const protect = require("static-auth");
 const safeCompare = require("safe-compare");
 
-const USER_NAME = process.env.USER_NAME || "admin"; // プロジェクトの環境変数を設定していた場合はそちらを適用させる
-const PASSWORD = process.env.PASSWORD || "admin";
+const USER_NAME = process.env.USER_NAME || "test"; // プロジェクトの環境変数を設定していた場合はそちらを適用させる
+const PASSWORD = process.env.PASSWORD || "test";
 
 const app = protect(
   "/",
   (username, password) =>
     safeCompare(username, USER_NAME) && safeCompare(password, PASSWORD), // timing attack 対策
   {
-    directory: `${__dirname}/storybook-static`, // storybook-static 配下のファイルを配信する
+    directory: `${__dirname}`, // storybook-static 配下のファイルを配信する
     onAuthFailed: (res) => {
       res.end("Authentication failed");
     },
